e299e2fcc0250421244ff682dddcdef78e18900d
ggroshansii <garthgroshansii@gmail.com>
2021-07-04 21:47:17 -0400

    added Book constructor and click eventhandler

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 
+function getRadioVal(form, name) {
+  let val;
+  // get list of radio buttons with specified name
+  let radios = form.elements[name];
+
+  // loop through list of radio buttons
+  for (let i = 0, len = radios.length; i < len; i++) {
+    if (radios[i].checked) { // radio checked?
+      val = radios[i].value; // if so, hold its value in val
+      break; // and break out of for loop
+    }
+  }
+  return val; // return value of checked radio or undefined if none checked
+}
+
+
+let myLibrary = [];
+
+function Book(author, title, pageCount, bookCover, read, index) {
+  this.author = author;
+  this.title = title;
+  this.pageCount = pageCount;
+  this.bookCover = bookCover;
+  this.read = read;
+  this.index = index;
+}
+
+//form fields
+const authorField = document.querySelector("#author");
+const titleField = document.querySelector("#title");
+const pageCountField = document.querySelector("#pageCount");
+const bookCoverField = document.querySelector("#bookCover");
+const readField = document.querySelector("#read");
+const notReadField = document.querySelector("#notRead");
+
+
+// Submit button
+const submitBook = document.querySelector("#submit");
+submitBook.addEventListener('click', function (e) {
+
+  e.preventDefault();
+  bookIndex = myLibrary.length;
+  const form = document.querySelector("#form");
+
+  const bookAddition = new Book(
+    authorField.value,
+    titleField.value,
+    pageCountField.value,
+    bookCoverField.value,
+    getRadioVal(form, 'haveRead'),
+    bookIndex);
+
+  myLibrary.push(bookAddition);
+  console.log(bookAddition);
+  console.log(myLibrary);
+})
+
+console.log(myLibrary)
